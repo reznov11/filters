345457beee0e91d4fd5220257f18459fd10003a5
Reznov Ammar <reznov110@gmail.com>
2018-10-30 03:41:46 +0600

    Props parent component

---
+++ src/components/elements/PropsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <input type="hidden" :value="name">
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:"props-cities-dropdown",
+        props: {
+            title: String,
+            name: String
+        },
+        data(){
+            return {};
+        }
+    }
+</script>
